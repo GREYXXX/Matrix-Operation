a5379f09e7cb79916fedb8cbf26e5daad8ebe6b3
GREYXXX <33743607+GREYXXX@users.noreply.github.com>
2019-09-11 19:29:04 +0800

    Update scalar.c

--- scalar.c
+++ scalar.c
@@ -3,7 +3,7 @@
 Matrix scalar_mul(char *optarg, Matrix a)
 {
 	float v = atof(optarg);
-	for(int i = 0; i < sizeof(a.data); i++)
+	for(int i = 0; i < a.value_num; i++)
 {
 	
 	a.data[i].value = (a.data[i].value) * v;
