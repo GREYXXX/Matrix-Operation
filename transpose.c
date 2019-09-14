327e5ed57acda836f047089ddc21fac1ea249d3b
GREYXXX <33743607+GREYXXX@users.noreply.github.com>
2019-09-14 22:23:19 +0800

    Update transpose.c

--- transpose.c
+++ transpose.c
@@ -1,6 +1,8 @@
 #include "variables.h"
 
-Matrix transposeMatrix(Matrix M,Matrix T){
+Matrix transposeMatrix(Matrix M){
+	Matrix T;
+	
 	T.col_num=M.row_num;
 	T.row_num=M.col_num;
 	T.value_num=M.value_num;
