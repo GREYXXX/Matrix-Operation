51ba5499a7c1ac275dc6b9a57398f3d19cdae197
msyukri-zaidin <msyukri.zaidin@gmail.com>
2019-09-18 11:06:15 +0800

    Readfile is now working

--- readfile.c
+++ readfile.c
@@ -1,5 +1,16 @@
 #include "variables.h"	
 
+char *removeSpaces(char *str)
+{
+	int count = 0;
+	for( int i = 0; str[i]; i++) 
+		if(str[i] != ' ')
+			str[count++] = str[i];
+	
+	str[count] = '\0';
+	return str;	
+}
+
 //This file reads the input and produces an internal representation for the matrix
 Matrix readfile(char *optarg) {
 	Matrix a;
@@ -24,9 +35,10 @@ Matrix readfile(char *optarg) {
 	int col = atoi(chr[2]); //Max number of columns
 	int k = 0;//counting the number of non-zero value
 	//int t = 0; 
-	
+/*	
 	for(int i = 0;i < row;i++) {
 		for(int j=0;j < col; j++) {
+				printf("Current iteration: %c\n", chr[3][k]);
 				a.data[k].row = i;
 				a.data[k].col = j;
 				if(chr[3][k]!='0'){
@@ -36,9 +48,37 @@ Matrix readfile(char *optarg) {
 				else	{continue;}
 		}
 	}
+*/
+	char *matrixLine = chr[3];
+	matrixLine = removeSpaces(matrixLine);
+
+	int count = 0;
+	//printf("%s\n", matrixLine);
+	int iteration = 0;
+	for( int i = 1; i <= col; i++) {
+		for(int j = 1; j <= row; j++) {
+			if(matrixLine[iteration] != '0') {
+				a.data[count].row = j;
+				a.data[count].col = i; 	
+				a.data[count].value = matrixLine[iteration] - '0';
+				count += 1;
+			} 
+			iteration += 1;
+		}
+	}
+	a.value_num = count;
+	a.row_num = row;
+	a.col_num = col;
+
+		
+	for( int i = 0; i < a.value_num; i++) {
+		printf("Value is: %d\n", a.data[i].value);
+	}	
+		
+
 	a.row_num = row; //Max number of rows
 	a.col_num = col; //Max number of columns
-	a.value_num = k + 1; //Number of non-zero values
+	a.value_num = count; //Number of non-zero values
         //fputs(chr,stdout);
         fclose(fp);
 	return a;
