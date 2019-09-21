38c5564f5fea7dbcd093106059bf6c26e2e7ff97
msyukri-zaidin <msyukri.zaidin@gmail.com>
2019-09-21 14:17:08 +0800

    Added dynamic allocation and further improvements to code

--- readfile.c
+++ readfile.c
@@ -14,7 +14,7 @@ char *removeSpaces(char *str)
 //This file reads the input and produces an internal representation for the matrix
 Matrix readfile(char *optarg) {
 	Matrix a;
-	//a.data = (Triple*)malloc(sizeof(Triple));
+	a.data = malloc(sizeof(Triple));
 
         char *chr[4]; //The buffer to store each line of the input file
 
@@ -23,13 +23,41 @@ Matrix readfile(char *optarg) {
                 perror("Unable to open file!");
                 exit(1);
      	}
-        int g=0;
+        int g = 0; //Character counter
+	int h = 0; //Line counter
+	char c; //The character
         char chunk[16384];	//Chunk represents a line in the input file
-	
+	char *dynamicChunk = malloc(256);
+/*	
 	while(fgets(chunk,sizeof(chunk), fp) != NULL) {
 		chr[g] = strdup(chunk);
 		g++;
 	}
+*/
+	while((c = fgetc(fp)) != EOF) {
+		if(h == 3) {
+			dynamicChunk[g] = c;
+			g++;
+			if((g % 256) == 0) {
+				dynamicChunk = realloc(dynamicChunk, g+256);	
+			}
+		}
+		else if(c == '\n') {
+			chunk[g] = '\0';
+			chr[h] = strdup(chunk);
+			g = 0;
+			h++;
+		}
+		else {
+			chunk[g] = c;
+			g++;
+		}
+	}
+	dynamicChunk[g] = '\0';
+	chr[h] = strdup(dynamicChunk);
+	//printf("%s\n", chr[3]);
+	printf("Allocated a total of %d bytes!\n", g+256);
+	free(dynamicChunk);
 /*
 	int c;
         while(fgets(chunk, sizeof(chunk), fp) != NULL){
@@ -62,7 +90,7 @@ Matrix readfile(char *optarg) {
 	//Current row and column count
 	int rowCount = 1;
 	int colCount = 1;
-	char storeNum[5];	
+	char storeNum[15]; //Stores the value in a character array
 
 	for(int i = 0; i < strlen(matrixLine) + 1; i++) {
 		//printf("%s\n", matrixLine);
@@ -72,12 +100,23 @@ Matrix readfile(char *optarg) {
 			colCount = 1;
 			rowCount++;
 		}
-		if(matrixLine[i] == '0' && numflag == false) {
+		if(matrixLine[i] == '.' && numflag == true) {
+			storeNum[numlen] = matrixLine[i];
+			numlen++;
+			continue;
+		}
+		else if(matrixLine[i] == '.') { //If it is 0.0 , continue
+			continue;
+		}
+		if(matrixLine[i] == '0' && numflag == false ) {
+			continue;
+/*
 			colCount++;	
 			if(colCount > col) {
 				colCount = 1;
 				rowCount++;
 			}
+*/
 		}
 		else if(matrixLine[i] == ' ' || matrixLine[i] == '\0') {
 			if(numflag == true) {
@@ -89,12 +128,15 @@ Matrix readfile(char *optarg) {
 				numflag = false;
 				numlen = 0;
 				count++;
-				//a.data = realloc(a.data, sizeof(Triple) * (count+1));
+				a.data = realloc(a.data, sizeof(Triple) * (count+1));
 				//printf("Memory allocated: %ld and value is: %f\n", sizeof(Triple) * (count+1), a.data[count-1].value);
 				colCount++;
-			}	
-			else {
 				continue;
+			}
+			colCount++;
+			if(colCount > col) {
+				colCount = 1;
+				rowCount++;	
 			}
 		}
 		else {
@@ -118,11 +160,10 @@ Matrix readfile(char *optarg) {
 	printf("Number of non-zero values: %i\n", a.value_num);
 */		
 
-        //fputs(chr,stdout);
         fclose(fp);
-	return a;
         for(int i = 0; i <4;i++) {
-        free(chr[i]);
+        	free(chr[i]);
 	}
+	return a;
 }
 
